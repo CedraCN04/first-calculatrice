efff555e831f829a16e7ffe96e051ef40f87c2e4
GuillaumeCeccoli <guiceccoli@gmail.com>
2025-05-12 18:15:44 +0200

    calculatrice sans interface

---
+++ script.js
@@ -0,0 +1,51 @@
+let operationChoice = prompt(
+  "Choisissez une opération : 1 - Addition, 2 - Soustraction, 3 - Multiplication, 4- Division"
+);
+
+function askUser() {
+  let firstNumber = Number(prompt("Entrez le premier nombre :"));
+  let secondNumber = Number(prompt("Entrez le deuxième nombre :"));
+  return [firstNumber, secondNumber];
+}
+
+function addition(firstNumber, secondNumber) {
+  let result = firstNumber + secondNumber;
+  alert("Le résultat de l'addition est : " + result);
+}
+
+function soustraction(firstNumber, secondNumber) {
+  let result = firstNumber - secondNumber;
+  alert("Le résultat de la soustraction est : " + result);
+}
+
+function multiplication(firstNumber, secondNumber) {
+  let result = firstNumber * secondNumber;
+  alert("Le résultat de la multiplication est : " + result);
+}
+
+function division(firstNumber, secondNumber) {
+  let result = firstNumber / secondNumber;
+  alert("Le résultat de la division est : " + result);
+}
+
+switch (operationChoice) {
+  case "1":
+    let addNumbers = askUser();
+    addition(addNumbers[0], addNumbers[1]);
+    break;
+  case "2":
+    let substractNumbers = askUser();
+    soustraction(substractNumbers[0], substractNumbers[1]);
+    break;
+  case "3":
+    let multiplyNumbers = askUser();
+    multiplication(multiplyNumbers[0], multiplyNumbers[1]);
+    break;
+  case "4":
+    let divideNumbers = askUser();
+    division(divideNumbers[0], divideNumbers[1]);
+    break;
+  default:
+    alert("Choix d'opération invalide. Veuillez réessayer.");
+}
+
