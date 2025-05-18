5c524e8c112638f57776a2e22fa2f8de29433d15
GuillaumeCeccoli <guiceccoli@gmail.com>
2025-05-18 18:19:34 +0200

    Ajout des fonctions principales

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Variables globales
+let firstValue = "";
+let secondValue = "";
+let operator = null;
+let isSecondValue = false;
+
 // Ajout d'une valeur par défaut
 function defaultValue() {
   let defaultValue = 0;
@@ -9,20 +15,96 @@ function defaultValue() {
 
 defaultValue();
 
-// Ajout d'une fonction pour effacer les données et revenir à la valeur par défaut
+// Fonction pour effacer les données et revenir à la valeur par défaut
 function clear() {
   let clearButton = document.getElementById("clear");
   clearButton.addEventListener("click", () => {
     defaultValue();
   });
 }
+clear();
 
-// Ajout d'une fonction pour supprimer le dernier caractère
+// Fonction pour supprimer le dernier caractère
 function back() {
   let backButton = document.getElementById("return");
-  backButton.addEventListener(click, () => {
+  backButton.addEventListener("click", () => {
     let screenResult = document.getElementById("result");
     screenResult.textContent = screenResult.textContent.slice(0, -1);
+    if (screenResult.textContent === "") {
+      defaultValue();
+    }
   });
 }
+
+back();
+
+// Fonction pour récupérer tous les boutons "number"
+function numbers() {
+  let numbers = document.querySelectorAll(".number");
+  numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+      let screenResult = document.getElementById("result");
+      if (screenResult.textContent === "0") {
+        screenResult.textContent = number.textContent;
+      } else {
+        screenResult.textContent += number.textContent;
+      }
+      screenResult.style.color = "black";
+      if (!isSecondValue) {
+        firstValue += number.textContent;
+      } else {
+        secondValue += number.textContent;
+      }
+    });
+  });
+}
+numbers();
+
+// Fonction pour récupérer tous les boutons "operator"
+function operators() {
+  let operators = document.querySelectorAll(".operator");
+  operators.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", () => {
+      let screenResult = document.getElementById("result");
+      screenResult.textContent += operatorButton.textContent;
+      operator = operatorButton.textContent;
+      screenResult.style.color = "black";
+      isSecondValue = true;
+    });
+  });
+}
+operators();
+
+// Fonction de calcul
+function calculate() {
+  let screenResult = document.getElementById("result");
+  let equalButton = document.getElementById("equal");
+  equalButton.addEventListener("click", () => {
+    switch (operator) {
+      case "+":
+        screenResult.textContent =
+          parseFloat(firstValue) + parseFloat(secondValue);
+        break;
+      case "-":
+        screenResult.textContent =
+          parseFloat(firstValue) - parseFloat(secondValue);
+        break;
+      case "*":
+        screenResult.textContent =
+          parseFloat(firstValue) * parseFloat(secondValue);
+        break;
+      case "/":
+        screenResult.textContent =
+          parseFloat(firstValue) / parseFloat(secondValue);
+        break;
+      default:
+        break;
+    }
+    firstValue = screenResult.textContent;
+    secondValue = "";
+    isSecondValue = false;
+    screenResult.style.color = "green";
+  });
+}
+calculate();
 
